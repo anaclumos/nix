785eb686dfd6d435751c212fe3443791b919b4ff
Sunghyun Cho <hey@cho.sh>
2026-01-02 20:06:02 +0900

    remove trayscale from autostart applications

--- home/autostart.nix
+++ home/autostart.nix
@@ -11,16 +11,6 @@
       Name=1Password
       Comment=Password manager and secure wallet
     '';
-    "autostart/trayscale.desktop".text = ''
-      [Desktop Entry]
-      Type=Application
-      Exec=${pkgs.trayscale}/bin/trayscale --hide-window
-      Hidden=false
-      NoDisplay=false
-      X-GNOME-Autostart-enabled=true
-      Name=Trayscale
-      Comment=Tailscale GUI
-    '';
     "1password/1password-bw-integration".text = "";
   };
 }
